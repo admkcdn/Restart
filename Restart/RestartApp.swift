15da382caad4b5e55e1a6dcfd77d60c3520fa207
adem <admkcdn@gmail.com>
2023-08-19 23:38:22 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Adem Koçdoğan on 19.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
